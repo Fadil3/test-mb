fd4d0ba28ca2b6283c31a5ade20f5d43b801213c
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-05-15 09:59:46 +0700

    add title html

--- index.php
+++ index.php
@@ -37,6 +37,9 @@ $i = 0;
   <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon//favicon-16x16.png">
 
+  <!-- title html -->
+  <title>Rumah Dekat MRT | MB Properti</title>
+
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" />
 
