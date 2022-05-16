a7d92cbc5b29300da03d658ed28a34be7cb15adf
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-05-16 13:54:12 +0700

    alert on send message

--- pesan.php
+++ pesan.php
@@ -3,17 +3,23 @@
 include('dbconnection.php');
 
 if (isset($_POST['submit'])) {
-    //set error message
+  //set error message
 
-    $nama = $_POST['nama'];
-    $email = $_POST['email'];
-    $telepon = $_POST['telepon'];
-    $subjek = $_POST['subjek'];
-    $pesan = $_POST['pesan'];
+  $nama = $_POST['nama'];
+  $email = $_POST['email'];
+  $telepon = $_POST['telepon'];
+  $subjek = $_POST['subjek'];
+  $pesan = $_POST['pesan'];
 
-    $sql = "INSERT INTO pesan(nama, email, telepon,subjek,pesan)
+  $sql = "INSERT INTO pesan(nama, email, telepon,subjek,pesan)
     VALUES ('$nama', '$email', '$telepon','$subjek','$pesan')";
-    $result = mysqli_query($db, $sql);
-    //redirect to login page
-    header('location: index.php');
+  $result = mysqli_query($db, $sql);
+
+  if ($result) {
+    echo "<script>alert('Pesan anda telah terkirim');</script>";
+    echo "<script>location.href='index.php';</script>";
+  } else {
+    echo "<script>alert('Pesan anda gagal terkirim');</script>";
+    echo "<script>location.href='index.php';</script>";
   }
+}
