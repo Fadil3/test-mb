f7c9bb2fdee577adaf5e752ad7b1fc81824f86aa
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-05-16 11:46:55 +0700

    Revert "fix pesan"
    This reverts commit 892268a4c4b9f038ce1abc860d83ce206b6c32a6.

--- lihat-pesan.php
+++ lihat-pesan.php
@@ -43,44 +43,44 @@ $i = 0;
   ?>
   <div class="container">
     <div class="d-flex justify-content-between">
-      <h1 class="mt-5">Pesan</h1>
+      <h1 class="mt-5">SEO</h1>
     </div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">No</th>
-          <th scope="col">Nama</th>
-          <th scope="col">Email</th>
-          <th scope="col">Telepon</th>
-          <th scope="col">Subjek</th>
-          <th scope="col">Pesan</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $ret = mysqli_query($db, "select * from pesan");
-        $i = 1;
-        while ($row = mysqli_fetch_array($ret)) { ?>
-          <th scope="row"><?php echo $i++ ?></th>
-          <td><?php echo $row['nama'] ?></td>
-          <td><?php echo $row['email'] ?></td>
-          <td><?php echo $row['telepon'] ?></td>
-          <td><?php echo $row['subjek'] ?></td>
-          <td><?php echo $row['pesan'] ?></td>
-        <?php
-        }
-        ?>
-        <tr>
-        </tr>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">Nama</th>
+            <th scope="col">Email</th>
+            <th scope="col">Telepon</th>
+            <th scope="col">Subjek</th>
+            <th scope="col">Pesan</th>
+          </tr>
+        </thead>
+        <tbody>
+            <?php 
+                $ret = mysqli_query($db, "select * from pesan");
+                $i = 1;
+                while ($row = mysqli_fetch_array($ret)) { ?>
+                    <th scope="row"><?php echo $i++ ?></th>
+                    <td><?php echo $row['nama'] ?></td>
+                    <td><?php echo $row['email'] ?></td>
+                    <td><?php echo $row['telepon'] ?></td>
+                    <td><?php echo $row['subjek'] ?></td>
+                    <td><?php echo $row['pesan'] ?></td>
+                <?php 
+                }
+                ?>
+          <tr>
+          </tr>
+           
+        </tbody>
+      </table>
 
-      </tbody>
-    </table>
+      <a href="logout.php" class="btn btn-danger float-right text-white mb-5">Logout</a>
+    </div>
 
-    <a href="index-admin.php" class="btn btn-primary float-right text-white mb-5">Kembali</a>
-  </div>
-
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 </body>
 
 </html>
